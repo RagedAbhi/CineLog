5dfd6ba42dbeccc436d96895e7b4c05c00ea0d2b
RagedAbhi <abhilash.khadanga@gmail.com>
2026-04-23 20:42:38 +0530

    feat: use redacted plot snippet for Hangman hint

--- server/controllers/gameController.js
+++ server/controllers/gameController.js
@@ -118,7 +118,7 @@ const getHangmanPuzzle = async (userId) => {
         wrongGuesses: 0,
         maxWrong: 6,
         turn: null, // Set by caller for multiplayer
-        hint: `${selected.genre} (${selected.year})`,
+        hint: selected.plot ? redactPlot(selected.plot.split('.')[0] + '.', selected.title) : `${selected.genre} (${selected.year})`,
         metadata: {
             poster: selected.poster,
             year: selected.year,
