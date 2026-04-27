4931e9e1f8d8b87caed4009021f9f7f191c04921
RagedAbhi <abhilash.khadanga@gmail.com>
2026-04-27 23:04:34 +0530

    Fix middleware function usage in playback routes

--- server/routes/playbackRoutes.js
+++ server/routes/playbackRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const playbackController = require('../controllers/playbackController');
-const auth = require('../middleware/authMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 // All playback routes require authentication
-router.post('/update', auth, playbackController.updateProgress);
-router.get('/all', auth, playbackController.getAllProgress);
-router.get('/:mediaId', auth, playbackController.getProgress);
-router.delete('/:mediaId', auth, playbackController.deleteProgress);
+router.post('/update', protect, playbackController.updateProgress);
+router.get('/all', protect, playbackController.getAllProgress);
+router.get('/:mediaId', protect, playbackController.getProgress);
+router.delete('/:mediaId', protect, playbackController.deleteProgress);
 
 module.exports = router;
